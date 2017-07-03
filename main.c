1a29db642ab32b52ad739b9e591fd27f109ba203
root <jetty840@users.noreply.github.com>
2017-07-03 11:37:44 -0600

    Updating to default to a 16 NeoPixel Ring: https://www.adafruit.com/product/1463

--- main.c
+++ main.c
@@ -69,7 +69,7 @@ static char VERSION[] = "XX.YY.ZZ";
 #define STRIP_TYPE              WS2811_STRIP_GBR		// WS2812/SK6812RGB integrated chip+leds
 //#define STRIP_TYPE            SK6812_STRIP_RGBW		// SK6812RGBW (NOT SK6812RGB)
 
-#define WIDTH                   12
+#define WIDTH                   16
 #define HEIGHT                  1
 #define LED_COUNT               (WIDTH * HEIGHT)
 
